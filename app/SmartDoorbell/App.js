c2732db9f1c0c0f637d811fe14f9655567d6beca
Chandler Mayo <chandler@chandlermayo.com>
2019-07-25 12:02:23 -0400

    Push notifications.

--- app/SmartDoorbell/App.js
+++ app/SmartDoorbell/App.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import {StyleSheet, FlatList, Text, View} from 'react-native';
 import PubNubReact from 'pubnub-react';
 
+import PushNotificationIOS from 'react-native';
+var PushNotification = require('react-native-push-notification');
+import PubNubReact from 'pubnub-react';
+
+
 type Props = {};
 export default class App extends Component<Props> {
   
@@ -18,7 +23,45 @@ export default class App extends Component<Props> {
         subscribeKey: 'YOUR_SUB_KEY_HERE' // YOUR PUBNUB SUBSCRIBE KEY HERE
     });
 
+    
+
     this.pubnub.init(this);
+
+    PushNotification.configure({
+      // Called when Token is generated.
+      onRegister: function(token) {
+          console.log( 'TOKEN:', token );
+          if (token.os == "ios") {
+            this.pubnub.push.addChannels(
+            {
+              channels: ['smart_buttons'],
+              device: token.token,
+              pushGateway: 'apns'
+            });
+            // Send iOS Notification from debug console: {"pn_apns":{"aps":{"alert":"Hello World."}}}
+          } else if (token.os == "android"){
+            this.pubnub.push.addChannels(
+            {
+              channels: ['smart_buttons'],
+              device: token.token,
+              pushGateway: 'gcm' // apns, gcm, mpns
+            });
+            // Send Android Notification from debug console: {"pn_gcm":{"data":{"message":"Hello World."}}}
+          }  
+      }.bind(this),
+      // Something not working?
+      // See: https://support.pubnub.com/support/solutions/articles/14000043605-how-can-i-troubleshoot-my-push-notification-issues-
+      // Called when a remote or local notification is opened or received.
+      onNotification: function(notification) {
+        console.log( 'NOTIFICATION:', notification );
+        // Do something with the notification.
+        // Required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+        // notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      // ANDROID: GCM or FCM Sender ID
+      senderID: "sender-id",
+  });
+}
   }
 
   componentDidMount() {
--- app/SmartDoorbell/ios/SmartDoorbell/AppDelegate.m
+++ app/SmartDoorbell/ios/SmartDoorbell/AppDelegate.m
@@ -11,6 +11,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <React/RCTPushNotificationManager.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -38,5 +40,7 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
+// Required to register for notifications - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings { [RCTPushNotificationManager didRegisterUserNotificationSettings:notificationSettings]; } // Required for the register event. - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken { [RCTPushNotificationManager didRegisterForRemoteNotificationsWithDeviceToken:deviceToken]; } // Required for the notification event. You must call the completion handler after handling the remote notification. - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler { [RCTPushNotificationManager didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler]; } // Required for the registrationError event. - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error { [RCTPushNotificationManager didFailToRegisterForRemoteNotificationsWithError:error]; } // Required for the localNotification event. - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification { [RCTPushNotificationManager didReceiveLocalNotification:notification]; }
 
 @end
